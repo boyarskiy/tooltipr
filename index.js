cb3e266ddf654f1881f6c46407ec5beaec26e127
Evgeniy Boyarskiy <gekabrs@gmail.com>
2015-06-17 10:27:33 +0300

    coords in a separate function

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 'use strict';
 
-module.exports = Tooltipr;
-
 function Tooltipr() {
     if (!(this instanceof Tooltipr)) return new Tooltipr();
 }
 
-Tooltipr.prototype.create = function(e) {
-    var current = e.target;
+Tooltipr.prototype.create = function() {
+    var current = event.target;
     this.tooltip = current.getAttribute('data-tooltip');
+
+    if(this.tooltip === null) {
+        return;
+    }
 
     this.tooltipDiv = document.createElement('div');
     this.tooltipDiv.className = 'tooltip';
     this.tooltipDiv.innerHTML = this.tooltip;
     document.body.appendChild(this.tooltipDiv);
 
-    var coords = current.getBoundingClientRect();
+    var getCoords = getTooltipCoords(this.tooltipDiv, current);
 
-    var left = coords.left + (current.offsetWidth - this.tooltipDiv.offsetWidth) / 2;
-    if (left < 0) left = 0;
-
-    var top = coords.top - this.tooltipDiv.offsetHeight - 5;
-    if (top < 0) top = coords.top + current.offsetHeight + 5;
-
-    this.tooltipDiv.style.left = left + 'px';
-    this.tooltipDiv.style.top = top + 'px';
+    this.tooltipDiv.style.left = getCoords.left + 'px';
+    this.tooltipDiv.style.top = getCoords.top + 'px';
 };
 
 Tooltipr.prototype.remove = function() {
@@ -32,3 +28,20 @@ Tooltipr.prototype.remove = function() {
         document.body.removeChild(this.tooltipDiv);
     }
 };
+
+function getTooltipCoords(tooltip, current) {
+    var tooltipCoords = current.getBoundingClientRect();
+
+    var left = tooltipCoords.left + (current.offsetWidth - tooltip.offsetWidth) / 2;
+    if (left < 0) left = 0;
+
+    var top = tooltipCoords.top - tooltip.offsetHeight - 5;
+    if (top < 0) top = tooltipCoords.top + current.offsetHeight + 5;
+
+    return {
+        top: top,
+        left: left
+    }
+}
+
+module.exports = Tooltipr;
